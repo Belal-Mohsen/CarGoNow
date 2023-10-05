7d771c0f10ca1086a7f95505163283d9b6cd354f
Belal Mohsen <belal4mohsen@gmail.com>
2023-10-05 16:49:11 -0400

    Added a plan for the data layer

---
+++ CarGoNowApp/Data/CarGoNowDBConnection.cs
@@ -0,0 +1,63 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CarGoNowApp.Data
+{
+    public class CarGoNowDBConnection
+    {
+        private void establishConnection()
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(get_ConnectionString()))
+                {
+                    conn.Open();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("DataBase cannot be connected! " + ex.Message);
+            }
+        }
+        private string get_ConnectionString()
+        {
+            string server = "localhost";
+            string database = "cargonowdb";
+            string uid = "root";
+            string pass = "Pass4Desk";
+
+            string constring = "Server=" + server + "; database=" + database + "; uid=" + uid + "; pwd=" + pass;
+            return constring;
+        }
+        // Deadline: Wed 11/Oct/2023
+        // for car, clients,employee, => Diana
+        // bill, res, login, delete for all => Belal
+        // showAll(whole table), update(row), insert(row), select(row)
+        // for all tables
+        // on delete
+        public void delete (int id, string tableName)
+        {
+
+        }
+        public void showAllEmployees()
+        {
+
+        }
+        public void AddEmployee(string name)
+        {
+
+        }
+        public void showAllCars()
+        {
+
+        }
+
+
+    }
+}
